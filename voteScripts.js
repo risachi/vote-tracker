c92907d682cc97b3d6a972d4fe3b7f4c986fef1e
Lisa Hackenberger <risachi@gmail.com>
2015-11-28 02:56:05 -0800

    first commit

---
+++ voteScripts.js
@@ -0,0 +1,31 @@
+var food = [
+  new voteImage("foodApple.jpg", images/foodApple.jpg),
+  new voteImage("foodCupTea.jpg", images/foodCupTea.jpg),
+  new voteImage("foodHamburgers.jpg", images/foodHamburgers.jpg),
+  new voteImage("foodHappyCandy.jpg", images/foodHappyCandy.jpg),
+  new voteImage("foodHappyEggs.jpg", images/foodHappyEggs.jpg),
+  new voteImage("foodHappyTofu.jpeg", images/foodHappyTofu.jpeg),
+  new voteImage("foodMarshmallows.jpg", images/foodMarshmallows.jpg),
+  new voteImage("foodMoreMarshmallows.jpg", images/foodMoreMarshmallows.jpg),
+  new voteImage("foodPenguins.jpg", images/foodPenguins.jpg),
+  new voteImage("foodStackMarshmallows.jpg", images/foodStackMarshmallows.jpg)
+];
+
+var pandas = [
+  new voteImage("axolotlPanda.jpg", images/axolotlPanda.jpg),
+  new voteImage("blepPanda.jpg", images/blepPanda.jpg),
+  new voteImage("curlyPanda.jpg", images/curlyPanda.jpg),
+  new voteImage("happyPanda.jpg", images/happyPanda.jpg),
+  new voteImage("laughyPandajpg", images/laughyPandajpg),
+  new voteImage("sadPanda.jpg", images/sadPanda.jpg),
+  new voteImage("sleepyPanda.jpg", images/sleepyPanda.jpg),
+  new voteImage("smirkyPanda.jpg", images/smirkyPanda.jpg),
+  new voteImage("stalkyPanda.jpg", images/stalkyPanda.jpg),
+  new voteImage("surprisePanda.jpg", images/surprisePanda.jpg),
+];
+
+function voteImage(name, location) = {
+  this.name,
+  this.location
+
+};
