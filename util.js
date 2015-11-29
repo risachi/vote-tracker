100f8e4871a6bc21eabe0191bc1a4baa205f5f03
Lisa Hackenberger <risachi@gmail.com>
2015-11-28 18:08:21 -0800

    takeOne function working

---
+++ util.js
@@ -0,0 +1,6 @@
+function takeOne(arr) {
+  // 1. choose a random item
+  // 2. delete it from the array
+  // 3. return the item
+  return arr[0]
+}
--- voteScripts.js
+++ voteScripts.js
@@ -1,31 +1,51 @@
+var foodClicks = 0
+var pandaClicks = 0
+var oneFood, onePanda;
+
 var food = [
-  new voteImage("foodApple.jpg", images/foodApple.jpg),
-  new voteImage("foodCupTea.jpg", images/foodCupTea.jpg),
-  new voteImage("foodHamburgers.jpg", images/foodHamburgers.jpg),
-  new voteImage("foodHappyCandy.jpg", images/foodHappyCandy.jpg),
-  new voteImage("foodHappyEggs.jpg", images/foodHappyEggs.jpg),
-  new voteImage("foodHappyTofu.jpeg", images/foodHappyTofu.jpeg),
-  new voteImage("foodMarshmallows.jpg", images/foodMarshmallows.jpg),
-  new voteImage("foodMoreMarshmallows.jpg", images/foodMoreMarshmallows.jpg),
-  new voteImage("foodPenguins.jpg", images/foodPenguins.jpg),
-  new voteImage("foodStackMarshmallows.jpg", images/foodStackMarshmallows.jpg)
+  new voteImage("foodApple.jpg", "images/foodApple.jpg"),
+  new voteImage("foodCupTea.jpg", "images/foodCupTea.jpg"),
+  new voteImage("foodHamburgers.jpg", "images/foodHamburgers.jpg"),
+  new voteImage("foodHappyCandy.jpg", "images/foodHappyCandy.jpg"),
+  new voteImage("foodHappyEggs.jpg", "images/foodHappyEggs.jpg"),
+  new voteImage("foodHappyTofu.jpeg", "images/foodHappyTofu.jpeg"),
+  new voteImage("foodMarshmallows.jpg", "images/foodMarshmallows.jpg"),
+  new voteImage("foodMoreMarshmallows.jpg", "images/foodMoreMarshmallows.jpg"),
+  new voteImage("foodPenguins.jpg", "images/foodPenguins.jpg"),
+  new voteImage("foodStackMarshmallows.jpg", "images/foodStackMarshmallows.jpg")
 ];
 
 var pandas = [
-  new voteImage("axolotlPanda.jpg", images/axolotlPanda.jpg),
-  new voteImage("blepPanda.jpg", images/blepPanda.jpg),
-  new voteImage("curlyPanda.jpg", images/curlyPanda.jpg),
-  new voteImage("happyPanda.jpg", images/happyPanda.jpg),
-  new voteImage("laughyPandajpg", images/laughyPandajpg),
-  new voteImage("sadPanda.jpg", images/sadPanda.jpg),
-  new voteImage("sleepyPanda.jpg", images/sleepyPanda.jpg),
-  new voteImage("smirkyPanda.jpg", images/smirkyPanda.jpg),
-  new voteImage("stalkyPanda.jpg", images/stalkyPanda.jpg),
-  new voteImage("surprisePanda.jpg", images/surprisePanda.jpg),
+  new voteImage("axolotlPanda.jpg", "images/axolotlPanda.jpg"),
+  new voteImage("blepPanda.jpg", "images/blepPanda.jpg"),
+  new voteImage("curlyPanda.jpg", "images/curlyPanda.jpg"),
+  new voteImage("happyPanda.jpg", "images/happyPanda.jpg"),
+  new voteImage("laughyPandajpg", "images/laughyPandajpg"),
+  new voteImage("sadPanda.jpg", "images/sadPanda.jpg"),
+  new voteImage("sleepyPanda.jpg", "images/sleepyPanda.jpg"),
+  new voteImage("smirkyPanda.jpg", "images/smirkyPanda.jpg"),
+  new voteImage("stalkyPanda.jpg", "images/stalkyPanda.jpg"),
+  new voteImage("surprisePanda.jpg", "images/surprisePanda.jpg"),
 ];
 
-function voteImage(name, location) = {
-  this.name,
-  this.location
-
+function voteImage(name, location) {
+  this.name = name
+  this.location = location
 };
+
+oneFood = takeOne(food)
+onePanda = takeOne(pandas)
+
+function showResults() {
+  //show total results of voting
+}
+
+function showChoice() {
+  window.print(oneFood, onePanda)
+}
+
+// if food.length = 0 {
+//   showResults()
+// } else {
+//   showChoice(oneFood, onePanda)
+// };
