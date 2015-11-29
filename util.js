d47d8370381f6055dfdb0c885931131c5f63cf9d
Lisa Hackenberger <risachi@gmail.com>
2015-11-28 21:34:32 -0800

    All tests pass

--- util.js
+++ util.js
@@ -2,12 +2,12 @@ function takeOne(arr) {
   if (arr.length === 0) {
     return null;
   } else {
-  // 1. choose a random item
-  var randomItem = arr[Math.floor(Math.random()*arr.length)];
-  // 2. delete it from the array
-  arr.splice(randomItem);
-  // 3. return the item
-  return randomItem;
+    var randomIndex = Math.floor(Math.random()*arr.length);
+    var randomItem  = arr[randomIndex];
+    // Delete it from the array
+    arr.splice(randomIndex, 1);
+    // Return the item
+    return randomItem;
   }
 }
 
